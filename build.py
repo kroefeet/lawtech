511ef0efe48f42f379a9f3598bb7162e8508483c
Joe Cera <subscribeto@runbox.com>
2019-06-21 13:49:58 -0700

    preparing and testing phase 2 of HW

--- build.py
+++ build.py
@@ -1,40 +1,52 @@
-from string import Template
-top_bottom_text = open('templates/top_bottom.html').read()
-template = Template(top_bottom_text)
-
-
-regenerate = input("Rgenerate site? (yes/no) ")
-
-if regenerate.lower() == 'yes':
-	#index page	
-	index_page = open('content/index.html').read()
-	index_html = template.safe_substitute(
-		title="Home",	
-		home_class="active",
-		content=index_page,
-	)	
-	open('docs/index.html', 'w+').write(index_html)
-	#projects page
-	projects_page = open('content/projects.html').read()
-	projects_html = template.safe_substitute(
-		title="Projects",	
-		projects_class="active",
-		content=projects_page,
-	)	
-	open('docs/projects.html', 'w+').write(projects_html)
-	#contact page
-	contact_page = open('content/contact.html').read()
-	contact_html = template.safe_substitute(
-		title="Contact me",	
-		contact_class="active",
-		content=contact_page,
-	)	
-	open('docs/contact.html', 'w+').write(contact_html)
+def main():
 	
-	print("Your site was regenerated. The new pages are in the docs/ folder")
-else:
-	print("Your site was not regenerated.")	
+	pages = [
+		{
+			"filename": "content/index.html",
+			"output": "docs/index.html",
+			"title": "Home",
+			"nav_class": "home",
+		},
+		{
+			"filename": "content/projects.html",
+			"output": "docs/projects.html",
+			"title": "Projects",
+			"nav_class": "projects",
+		},
+		{
+			"filename": "content/contact.html",
+			"output": "docs/contact.html",
+			"title": "Contact me",
+			"nav_class": "contact",
+		},	
 	
-
+	]
+	
+	
+	from string import Template
+	top_bottom_text = open('templates/top_bottom.html').read()
+	template = Template(top_bottom_text)
+	
+	
+	regenerate = input("Rgenerate site? (yes/no) ")
+	
+	if regenerate.lower() == 'yes':
+		for page in pages:
+			new_page = open(page["filename"]).read()	
+			page_html = template.safe_substitute(
+				title=page["title"],
+				content=new_page
+				page["nav_class"]+"class"="active"
+							
+			)	
+			open(page["output"], 'w+').write(page_html)
+		
+		
+		print("Your site was regenerated. The new pages are in the docs/ folder")
+	else:
+		print("Your site was not regenerated.")	
+		
+if __name__ == "__main__":
+	main()
 
 
