899dd23306803028870cabc302c399ea90a45354
Joe Cera <subscribeto@runbox.com>
2019-06-20 17:02:03 -0700

    making an attempt at templating with one file

--- build.py
+++ build.py
@@ -1,12 +1,46 @@
-top = open('templates/top.html').read()
-bottom = open('templates/bottom.html').read()
+from string import Template
+top_bottom_text = open('top_bottom.html').read()
+template = Template(top_bottom_text)
 
-page_name = input("What is the name of your content file? ")
 
-page = open('content/'+page_name).read()
-#print(page)
-# print(top)
-new_page = (top + page + bottom)
+top_bottom = open('templates/top_bottom.html').read()
 
-open('docs/'+page_name, 'w+').write(new_page)
-print("Your page should now be available here: docs/{}".format(page_name))
+regenerate = input("Rgenerate site? (yes/no) ")
+
+if regenerate.lower() == 'yes':
+	
+	#index page	
+	index_page = open('content/index.html').read()
+	index_html = template.safe_substitute(
+		title="Home",	
+		index_class="active",
+		content=index_page,
+	)	
+	open('docs/index.html', 'w+').write(index_html)
+
+	#projects page
+	projects_page = open('content/projects.html').read()
+	projects_html = template.safe_substitute(
+		title="Projects",	
+		projects_class="active",
+		content=projects_page,
+	)	
+	open('docs/projects.html', 'w+').write(projects_html)
+
+	#contact page
+	contact_page = open('content/contact.html').read()
+	contact_html = template.safe_substitute(
+		title="Contact me",	
+		contact_class="active",
+		content=contact_page,
+	)	
+	open('docs/contact.html', 'w+').write(contact_html)
+	
+	print("Your site was regenerated. The new pages are in the docs/ folder")
+	
+else:
+	print("Your site was not regenerated.")	
+	break
+
+
+
